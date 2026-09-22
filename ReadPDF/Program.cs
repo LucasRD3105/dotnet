94150eab1874fdbcaa86c3cfcd72e8b3749a001a
LucasRD3105 <lucasrdprofissional@gmail.com>
2026-09-22 18:14:48 -0300

    .

---
+++ ReadPDF/Program.cs
@@ -0,0 +1,15 @@
+﻿using EasyAutomationFramework;
+using System;
+
+namespace ReadPDF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Base.ExtractTextPdf("file:///C:/Users/Lucas Rocha Dantas/Documents/Lucas Rocha Dantas/Vida profissional/Certificados/Fundamentos do C%23 - Balta.pdf");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
